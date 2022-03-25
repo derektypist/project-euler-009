8ccbdddc4733ac894d347b844b969ee95b2d72c7
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-03-25 12:01:48 +0000

    Updated script.js

--- js/script.js
+++ js/script.js
@@ -5,8 +5,8 @@ function getNumberInfo() {
     // Get the Value of the Input Field
     let num = document.getElementById("mynumber").value;
     // Check if the input is valid
-    if (isNaN(num) || num.toString().length == 0 || num<6 || num>10000 || !Number.isInteger(Number(num))) {
-        txt += `Invalid Input.  Please enter a whole number between 6 and 10000.`;
+    if (isNaN(num) || num.toString().length == 0 || num<12 || num>10000 || !Number.isInteger(Number(num))) {
+        txt += `Invalid Input.  Please enter a whole number between 12 and 10000.`;
     } else {
         txt += `You have entered the number ${num}. <p>`;
         txt += `Product is ${specialPythagoreanTriplet(num)}.`;
